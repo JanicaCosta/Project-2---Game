650d18e4fe3fe882200b3bef72440a2ed3275aa8
JanicaCosta <joaocarocco@gmail.com>
2023-03-07 15:04:29 +0000

    functions to check for the winner , running the combinations const... and check for a draw, when all 9 spaces are filled in and none is a winner combination

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,19 @@ let checkTurn = true ;
 let playerX = "X";
 let playerO = "O";
 
+/**winning combinations */
+
+let winnerCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
 /**defining event when clicking in the space for the board to show the players */
 
 document.addEventListener("click", myfunction);
@@ -28,14 +41,41 @@ function startGame(id) {
     turn = checkTurn ? playerX : playerO;
     cell.textContent = turn;
     checkTurn = !checkTurn;
+    cell.classList.add(turn);
+    checkWinner(turn);
 };
 
 function checkWinner() {
+    const winner = winnerCombinations.some((comb) => {
+        return comb.every((index) => {
+          return cells[index].classList.contains(turn);
+        });
+      });
 
+      if (winner) {
+        gameCompleted(turn);
+      } else if (checkDraw()) {
+        gameCompleted();
+      } else {
+        checkTurn = !checkTurn;
+      }
 };
 
 function checkDraw() {
+    let x = 0;
+    let o = 0;
+    for (index in cells) {
+      if (!isNaN(index)) {
+        if (cells[index].classList.contains(playerX)) {
+          x++;
+        }
 
+        if (cells[index].classList.contains(playerO)) {
+          o++;
+        }
+      }
+    }
+    return x + o === 9 ? true : false;
 }
 
 function gameCompleted() {
